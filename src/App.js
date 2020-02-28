55ff26f7772b8910ff6181cb4816bb609f1976f1
Raphael Brand <raphael-brand@users.noreply.github.com>
2020-02-28 19:49:41 +0100

    package bundler 'parcel' added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hellor World</h1>
+  const message = "It's a wrap";
+  const element = <p>{message}</p>;
+  return (
+    <div>
+      <h1>Hellor World</h1>
+      <div>{element}</div>
+    </div>
+  );
 }
 
 export default App;
